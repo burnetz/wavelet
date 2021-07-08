6ea29c8dba14a0ed4fdc51dfb96acec6dd1a94b3
burnetz <32961116+burnetz@users.noreply.github.com>
2021-07-08 23:43:27 +0900

    wavファイルのデータを読み込むためのプログラムを追加

---
+++ ReadWav.py
@@ -0,0 +1,47 @@
+# 参考 : https://qiita.com/futurebone/items/fd42892c237a817cdbb4
+
+import numpy as np
+import wave
+import struct
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+# ファイルを読み出し
+#print("Please Enter the filename")
+#wavf = input("->")
+wavf = "./asset/voice.wav"
+wr = wave.open(wavf, 'r')
+
+# waveファイルが持つ性質を取得
+ch = wr.getnchannels()
+width = wr.getsampwidth()
+fr = wr.getframerate()
+fn = wr.getnframes()
+
+print("Channel: ", ch)
+print("Sample width: ", width)
+print("Frame Rate: ", fr)
+print("Frame num: ", fn)
+print("Params: ", wr.getparams())
+print("Total time: ", 1.0 * fn / fr)
+
+# waveの実データを取得し、数値化
+data = wr.readframes(wr.getnframes())
+wr.close()
+
+# フレーム数は2のべき乗にする
+fn = 512
+X = np.arange(0,fn,1)
+Y = np.frombuffer(data, dtype=np.int16)
+Y1 = Y[0:fn]
+
+# 読み込み結果をグラフで確認したいとき
+# plt.plot(X,Y1)
+# plt.savefig('figure.png')
+
+# テキストファイルに出力
+with open('wave.txt', 'w') as f:
+    f.write("%d\n" % fn)
+    for d in Y1:
+        f.write("%d\n" % d)
