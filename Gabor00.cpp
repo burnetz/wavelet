b41c8edc876142448b0c34e980b0a0aaa0e150f8
burnetz <32961116+burnetz@users.noreply.github.com>
2021-07-24 23:44:56 +0900

    ガボールのウェーブレットのためのプログラムを追加

---
+++ Gabor00.cpp
@@ -0,0 +1,130 @@
+#include <stdio.h>
+#include <math.h>
+#include "Gabor00.h"
+
+#define _PI 3.14159265358979323846264338327950288419716939937510
+#define WIN_RATE 0.01
+
+CGabor00::CGabor00()
+{
+    for (int i = 0; i < 8; i++)
+    {
+        m_pWork[i] = NULL;
+    }
+}
+
+CGabor00::~CGabor00()
+{
+    for (int i = 0; i < 8; i++)
+    {
+        if (m_pWork[i] != NULL)
+        {
+            delete m_pWork[i];
+        }
+    }
+}
+
+bool CGabor00::Prepare(double *input, int inputN)
+{
+    m_pInput = input;
+    m_nInputN = inputN;
+
+    return true;
+}
+
+bool CGabor00::SetParameter(double sigma, double omega, double a){
+    bool ret = false;
+
+    for(int i = 0; i < 2; i++){
+        if(m_pWork[i] != NULL){
+            delete m_pWork[i];
+        }
+        m_pWork[i] = NULL;
+    }
+    if(sigma <= 0.0 || omega <= 0.0 || a <= 0.0){
+        return ret;
+    }
+
+    m_nGaborOffset = (int)(sqrt(-2.0*sigma*sigma*a*a*log(WIN_RATE)));
+    m_nGaborN = m_nGaborOffset*2 + 1;
+
+    int i;
+    for(i = 0; i < 2; i++){
+        m_pWork[i] = new double[m_nGaborN];
+        if(m_pWork[i] == NULL){
+            break;
+        }
+    }
+
+    if(i == 2){
+        m_pGaborR = m_pWork[0];
+        m_pGaborI = m_pWork[1];
+        ret = true;
+    }
+
+    if(ret == true){
+        for(i = 0 ; i < m_nGaborN; i++){
+            double x = (double)(i - m_nGaborOffset)/a;
+            double d = exp(-x*x/(2.0*sigma*sigma))/sqrt(2.0*_PI*sigma*sigma);
+            m_pGaborR[i] = d*cos(omega*x)/sqrt(a);
+            m_pGaborI[i] = d*sin(omega*x)/sqrt(a);
+        }
+    }
+
+    return ret;
+
+}
+
+double CGabor00::Gabor(int b){
+    if(m_pWork[0] == NULL){
+        return 0.0;
+    }
+
+    double dr = 0.0;
+    double di = 0.0;
+
+    double real2, imag2;
+    for(int i = -(m_nGaborN - 1)/2; i<= (m_nGaborN - 1)/2; i++){
+        double real0 = ReadInput(b + i);
+        double imag0 = 0.0;
+
+        double real1 = ReadGaborR(i);
+        double imag1 = ReadGaborI(i);
+
+        ComplexMultiply(real2, imag2, real0, imag0, real1, -imag1);
+
+        dr += real2;
+        di += imag2;
+    }
+    return dr*dr + di*di;
+}
+
+double CGabor00::ReadInput(int x){
+    if(x < 0 || x >= m_nInputN){
+        return 0.0;
+    }
+    return m_pInput[x];
+}
+
+double CGabor00::ReadGaborR(int x){
+    x += m_nGaborOffset;
+
+    if(x < 0 || x >= m_nGaborN){
+        return 0.0;
+    }
+    return m_pGaborR[x];
+}
+
+double CGabor00::ReadGaborI(int x){
+    x += m_nGaborOffset;
+
+    if(x < 0 || x >= m_nGaborN){
+        return 0.0;
+    }
+    return m_pGaborI[x];
+}
+
+void CGabor00::ComplexMultiply(double &rOutput, double &iOutput, double rInput0, double iInput0, double rInput1, double iInput1){
+    rOutput = rInput0*rInput1 - iInput0*iInput1;
+    iOutput = iInput0*rInput1 + rInput0*iInput1;
+}
---
+++ Gabor00.h
@@ -0,0 +1,29 @@
+class CGabor00
+{
+public:
+    CGabor00();
+    ~CGabor00;
+
+    bool Prepare(double *input, int inputN);
+    bool SetParameter(double sigma, double omega, double a);
+    double Gabor(int b);
+
+protected:
+    double *m_pWork[8];
+    double *m_pInput;
+    int m_nInputN;
+
+    double m_nSigma;
+    double m_nOmega;
+    double m_nA;
+
+    double *m_pGaborR;
+    double *m_pGaborI;
+    int m_nGaborOffset;
+    int m_nGaborN;
+
+    double ReadInput(int x);
+    double ReadGaborR(int x);
+    double ReadGaborI(int x);
+    void ComplexMultiply(double &rOutput, double &iOutput, double rInput0, double iInput0, double rInput1, double iInput1);
+};
